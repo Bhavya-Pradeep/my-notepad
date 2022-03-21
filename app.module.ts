1c36d4a95c6406ef5726994f7d43e4a8dff6da45
bhavyaPradeep <bhavya.kundapura@gmail.com>
2022-03-21 22:20:02 +0530

    first commit

---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NotesService } from './notes.service';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+     
+  ],
+  providers:[NotesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
